2a9b6c0bc14221ce1c41687f18bac45f870bc07a
Milan-Sharma-449 <milansharma449@gmail.com>
2022-12-26 19:35:08 +0530

    made searchbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import { Stack } from '@mui/material'
-import { Link } from 'react-router-dom'
-import { logo } from '../utils/constants'
-import SearchBar from './SearchBar'
+import { Stack } from "@mui/material";
+import { Link } from "react-router-dom";
 
-const NavBar = () => {
-    <Stack direction='row' alignItems='center' p={2} sx={{position: 'sticky', background: '#000', top: 0, justifyContent: 'space-between'}}>
-      <Link to='/' style={{ display: 'flex', alignItems: 'center' }}>
-        <img src={logo} alt='logo' height={45} />
-      </Link>
-      <SearchBar />
-    </Stack>
-}
+import { logo } from "../utils/constants";
+import { SearchBar } from "./";
 
-export default NavBar
+const NavBar = () => (
+  <Stack direction="row" alignItems="center" p={2} sx={{ position:  "sticky", background: '#000', top: 0, justifyContent: "space-between" }}>
+    <Link to="/" style={{ display: "flex", alignItems: "center" }}>
+      <img src={logo} alt="logo" height={45} />
+    </Link>
+    <SearchBar />
+  </Stack>
+);
+
+export default NavBar;
