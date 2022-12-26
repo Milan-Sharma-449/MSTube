a9a6109935501a56850d85414f75470f22e8cfb9
Milan-Sharma-449 <milansharma449@gmail.com>
2022-12-26 19:26:46 +0530

    fixed errors

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { Box } from '@mui/material'
-import { NavBar, Feed, VideoDetail, ChannelDetail, SearchFeed } from './components'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from '@mui/material';
 
-const App = () => {
+import { ChannelDetail, VideoDetail, SearchFeed, NavBar, Feed } from './components';
+
+const App = () => (
   <BrowserRouter>
-    <Box sx={{ backgroundColor: '#000'}}>
+    <Box sx={{ backgroundColor: '#000' }}>
       <NavBar />
       <Routes>
-        <Route path='/' exact element={<Feed />} />
+        <Route exact path='/' element={<Feed />} />
         <Route path='/video/:id' element={<VideoDetail />} />
         <Route path='/channel/:id' element={<ChannelDetail />} />
-        <Route path='/search/:id' element={<SearchFeed />} />
+        <Route path='/search/:searchTerm' element={<SearchFeed />} />
       </Routes>
     </Box>
   </BrowserRouter>
-}
+);
 
-export default App
+export default App;
